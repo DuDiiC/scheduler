77749dc52d7ae5ffa505741a0c2b1d86b0ee377c
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-03-22 17:43:05 +0100

    Add login dto

---
+++ src/main/java/com/md/scheduler/configuration/security/AuthCredentials.java
@@ -0,0 +1,11 @@
+package com.md.scheduler.configuration.security;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+class AuthCredentials {
+
+    String username;
+    String password;
+}
