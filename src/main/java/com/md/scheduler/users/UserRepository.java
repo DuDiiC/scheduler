8660df5b515169a744c6d4743c35b86fc3cd6ff2
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-03-24 15:48:14 +0100

    Add new methods into user repository
    - findByEmail to check uniqueness
    - save to create new user in registration process

--- src/main/java/com/md/scheduler/users/UserRepository.java
+++ src/main/java/com/md/scheduler/users/UserRepository.java
@@ -5,4 +5,8 @@ import java.util.Optional;
 public interface UserRepository {
 
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    User save(User user);
 }
