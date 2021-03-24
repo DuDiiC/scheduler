c277a5ee859e3f1e6f4ddbad01755a8114df4291
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-03-24 16:25:49 +0100

    Remove unnecessary imports

--- src/main/java/com/md/scheduler/configuration/security/LoginControllerImpl.java
+++ src/main/java/com/md/scheduler/configuration/security/LoginControllerImpl.java
@@ -2,8 +2,6 @@ package com.md.scheduler.configuration.security;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-
 @RestController
 class LoginControllerImpl implements LoginController {
 
--- src/main/java/com/md/scheduler/users/User.java
+++ src/main/java/com/md/scheduler/users/User.java
@@ -1,12 +1,11 @@
 package com.md.scheduler.users;
 
+import com.md.scheduler.configuration.security.AppUserRole;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Objects;
 
 @Entity
