7e66220c288aab8756d44b8929b43147c48c31d4
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-03-30 22:03:13 +0200

    Add `findById()` in user repo

--- src/main/java/com/md/scheduler/users/UserRepository.java
+++ src/main/java/com/md/scheduler/users/UserRepository.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 public interface UserRepository {
 
+    Optional<User> findById(long id);
+
     Optional<User> findByUsername(String username);
 
     Optional<User> findByEmail(String email);
