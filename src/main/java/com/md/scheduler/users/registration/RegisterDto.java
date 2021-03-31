b65e6041a1b188a189d551581c42842d91c7aed2
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-03-31 17:46:30 +0200

    Add validation for registration DTO (without password)

--- src/main/java/com/md/scheduler/users/registration/RegisterDto.java
+++ src/main/java/com/md/scheduler/users/registration/RegisterDto.java
@@ -2,10 +2,20 @@ package com.md.scheduler.users.registration;
 
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
 @Getter
 class RegisterDto {
 
+    @Email(message = "Email must be in correct format")
     String email;
+
+    @Size(
+            min = 5, max = 100,
+            message = "Username must be in correct size (between 5 and 100 chars"
+    )
     String username;
+    
     String password;
 }
