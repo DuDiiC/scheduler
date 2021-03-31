e9f84b8acbb676f7488050245d7b8b0e61edbff6
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-03-31 11:34:06 +0200

    Replace repo to service in user controller and add exception handling

--- src/main/java/com/md/scheduler/users/UserControllerImpl.java
+++ src/main/java/com/md/scheduler/users/UserControllerImpl.java
@@ -1,24 +1,27 @@
 package com.md.scheduler.users;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
+import javax.persistence.EntityNotFoundException;
 
 @RestController
 @RequiredArgsConstructor
 class UserControllerImpl implements UserController {
 
-    private final UserRepository repository;
+    private final UserService service;
 
     @Override
     public ResponseEntity<?> getUser(Long id) {
-        Optional<User> user = repository.findById(id);
-        if (user.isPresent()) {
-            return ResponseEntity.ok(new UserInfo(user.get()));
-        } else {
-            return ResponseEntity.notFound().build();
+        try {
+            return ResponseEntity.ok(
+                    new UserInfo(service.getById(id))
+            );
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
     }
 }
