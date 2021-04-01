687235049d29022af433d5b7627e7f105f0105aa
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-04-01 13:19:42 +0200

    Rebuild ApiException dto

--- src/main/java/com/md/scheduler/configuration/exception/ApiException.java
+++ src/main/java/com/md/scheduler/configuration/exception/ApiException.java
@@ -1,15 +1,33 @@
 package com.md.scheduler.configuration.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Builder
 @Getter
-public class ApiException {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+class ApiException {
 
-    private String type;
-    private String title;
-    private Integer status;
-    private String detail;
-    private String instance;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:dd:ss")
+    private final LocalDateTime timestamp;
+    private final String status;
+    private final String message;
+    private List<ApiSubException> details;
+    private final String debugMessage;
+
+    private void addSubException(ApiSubException ex) {
+        if (this.details == null) {
+            this.details = new ArrayList<>();
+        }
+        details.add(ex);
+    }
+
 }
