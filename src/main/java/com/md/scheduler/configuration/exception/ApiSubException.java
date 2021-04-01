d5918ae004f6e63dd80482e9d6af881f5ffed658
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-04-01 13:08:37 +0200

    Add subexception interface to store error details

---
+++ src/main/java/com/md/scheduler/configuration/exception/ApiSubException.java
@@ -0,0 +1,7 @@
+package com.md.scheduler.configuration.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+interface ApiSubException {
+}
