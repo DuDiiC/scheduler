6eeea684eb9b04592b0827c244474914ba91fe58
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-04-01 11:27:06 +0200

    Use @Size and @Pattern annotation instead of own @Password

--- src/main/java/com/md/scheduler/users/registration/RegisterDto.java
+++ src/main/java/com/md/scheduler/users/registration/RegisterDto.java
@@ -3,6 +3,7 @@ package com.md.scheduler.users.registration;
 import lombok.Getter;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -17,15 +18,15 @@ class RegisterDto {
     )
     String username;
 
-    @Password(
-            minLength = 8,
-            maxLength = 24,
-            uppercaseCharRequired = true,
-            lowercaseCharRequired = true,
-            digitRequired = true,
-            specialCharRequired = true,
-            message = "The password must be 8-24 characters long, with at least one lowercase, " +
-                "one uppercase letter, one number and one special character"
+    @Size(
+            min = 8, max = 24,
+            message = "password must be 8-24 characters long"
     )
+    @Pattern.List({
+            @Pattern(regexp = ".*[a-z].*", message = "password must contains at least one lowercase"),
+            @Pattern(regexp = ".*[A-Z].*", message = "password must contains at least one lowercase"),
+            @Pattern(regexp = ".*\\d.*", message = "password must contains at least one digit"),
+            @Pattern(regexp = ".*[!@#$%&*()_+=|<>?{}\\\\[\\\\]~-].*", message = "password must contains at least one special character")
+    })
     String password;
 }
