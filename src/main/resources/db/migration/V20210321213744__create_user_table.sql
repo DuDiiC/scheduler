1642909437ddbd5da02153811884134409dc4d10
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-05-03 16:26:48 +0200

    Correct formatting in sql

--- src/main/resources/db/migration/V20210321213744__create_user_table.sql
+++ src/main/resources/db/migration/V20210321213744__create_user_table.sql
@@ -1,10 +1,11 @@
-drop table if exists users;
+drop table if exists "users";
 
-create table users (
-    id bigint primary key auto_increment,
-    email varchar2(100) not null,
+create table "users"
+(
+    id       bigint primary key auto_increment,
+    email    varchar2(100) not null,
     username varchar2(100) not null,
     password varchar2(255) not null,
-    role varchar2(100) not null,
-    enabled bit not null
+    role     varchar2(100) not null,
+    enabled  bit           not null
 )
