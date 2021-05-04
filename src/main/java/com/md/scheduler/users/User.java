18363af31dc01420abdc71ba6db97ca64d8d4e7b
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-05-04 11:55:10 +0200

    Remove implementing UserDetails from User entity

--- src/main/java/com/md/scheduler/users/User.java
+++ src/main/java/com/md/scheduler/users/User.java
@@ -2,16 +2,16 @@ package com.md.scheduler.users;
 
 import com.md.scheduler.commons.BaseEntity;
 import com.md.scheduler.configuration.security.AppUserRole;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.*;
-import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
 import java.util.Objects;
 
 @Entity
 @Table(name = "users")
-public class User extends BaseEntity implements UserDetails {
+public class User extends BaseEntity {
 
     private String username;
     private String password;
@@ -35,41 +35,18 @@ public class User extends BaseEntity implements UserDetails {
         return email;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return role.getGrantedAuthorities();
-    }
-
     public AppUserRole getRole() {
         return role;
     }
 
-    @Override
     public String getPassword() {
         return password;
     }
 
-    @Override
     public String getUsername() {
         return username;
     }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
     public boolean isEnabled() {
         return enabled;
     }
