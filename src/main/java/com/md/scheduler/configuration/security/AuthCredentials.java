614026c3ea6d3fe17b9671b808983921e3a1ad2f
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-05-05 11:35:17 +0200

    Needed constructors for auth dto

--- src/main/java/com/md/scheduler/configuration/security/AuthCredentials.java
+++ src/main/java/com/md/scheduler/configuration/security/AuthCredentials.java
@@ -1,8 +1,12 @@
 package com.md.scheduler.configuration.security;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 class AuthCredentials {
 
     String username;
