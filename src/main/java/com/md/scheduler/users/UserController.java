902bef13cf6c9e1167dd8bf46951bd8b9e6fc684
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-05-17 09:23:39 +0200

    In OpenAPI add security for endpoint based on bearer token

--- src/main/java/com/md/scheduler/users/UserController.java
+++ src/main/java/com/md/scheduler/users/UserController.java
@@ -1,6 +1,8 @@
 package com.md.scheduler.users;
 
 import com.md.scheduler.configuration.api.errors.EntityNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,5 +15,6 @@ interface UserController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
+    @Operation(security = { @SecurityRequirement(name = "bearer-key") })
     UserInfo getUser(@PathVariable Long id) throws EntityNotFoundException;
 }
