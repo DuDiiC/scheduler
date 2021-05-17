be4606099d67767408388e5921329fb81a29c926
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-05-17 08:37:36 +0200

    Add group with API in version 1

--- src/main/java/com/md/scheduler/configuration/swagger/SwaggerConfig.java
+++ src/main/java/com/md/scheduler/configuration/swagger/SwaggerConfig.java
@@ -9,6 +9,14 @@ import org.springframework.context.annotation.Profile;
 class SwaggerConfig {
 
     @Bean
+    GroupedOpenApi apiV1() {
+        return GroupedOpenApi.builder()
+                .group("API v1")
+                .pathsToMatch("/api/v1/**")
+                .build();
+    }
+
+    @Bean
     @Profile("!prod")
     GroupedOpenApi actuatorApi() {
         return GroupedOpenApi.builder()
