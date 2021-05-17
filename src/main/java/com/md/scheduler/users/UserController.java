c12a93cd0ffc4f9fb40d7f82cd36c761aeb85f37
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-05-17 09:22:37 +0200

    Add tags for existing endpoints

--- src/main/java/com/md/scheduler/users/UserController.java
+++ src/main/java/com/md/scheduler/users/UserController.java
@@ -1,12 +1,14 @@
 package com.md.scheduler.users;
 
 import com.md.scheduler.configuration.api.errors.EntityNotFoundException;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api/v1/users")
+@Tag(name = "user")
 interface UserController {
 
     @PreAuthorize("hasRole('ADMIN')")
--- src/main/java/com/md/scheduler/users/registration/RegisterController.java
+++ src/main/java/com/md/scheduler/users/registration/RegisterController.java
@@ -1,5 +1,6 @@
 package com.md.scheduler.users.registration;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +10,7 @@ import javax.validation.Valid;
 import java.io.Serializable;
 
 @RequestMapping("/api/v1")
+@Tag(name = "registration", description = "registration API to create new user")
 interface RegisterController {
 
     @PostMapping("/register")
