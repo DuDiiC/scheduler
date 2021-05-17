9ed7314e869a338cc4b18b10ca5bb2b3bad9a332
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-05-17 08:40:37 +0200

    Add bearer token authentication in OpenAPI doc

--- src/main/java/com/md/scheduler/configuration/swagger/SwaggerConfig.java
+++ src/main/java/com/md/scheduler/configuration/swagger/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.md.scheduler.configuration.swagger;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +10,19 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 class SwaggerConfig {
+
+    @Bean
+    OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes(
+                        "bearer-key",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                );
+    }
 
     @Bean
     GroupedOpenApi apiV1() {
