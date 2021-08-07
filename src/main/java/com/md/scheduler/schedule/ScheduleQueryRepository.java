98f1329fa3c1ec01c2dae7601453f08925ba06f6
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-08-07 18:35:26 +0200

    Added repository method for checking schedule duplicates

--- src/main/java/com/md/scheduler/schedule/ScheduleQueryRepository.java
+++ src/main/java/com/md/scheduler/schedule/ScheduleQueryRepository.java
@@ -1,5 +1,6 @@
 package com.md.scheduler.schedule;
 
+import com.md.scheduler.users.User;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
@@ -9,5 +10,7 @@ public interface ScheduleQueryRepository extends Repository<Schedule, Long> {
 
     Optional<Schedule> findById(long id);
 
+    boolean existsByNameAndOwner(String name, User owner);
+    
     List<Schedule> findAll();
 }
