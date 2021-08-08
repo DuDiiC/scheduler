e5a348359b59b9889e5f3612929c13db124313fc
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-08-08 18:19:23 +0200

    Added OpenAPI doc for basic schedule API

--- src/main/java/com/md/scheduler/schedule/ScheduleApi.java
+++ src/main/java/com/md/scheduler/schedule/ScheduleApi.java
@@ -1,42 +1,93 @@
 package com.md.scheduler.schedule;
 
+import com.md.scheduler.configuration.api.errors.ApiError;
 import com.md.scheduler.configuration.api.errors.EntityNotFoundException;
 import com.md.scheduler.configuration.api.errors.ResourceAlreadyExistsException;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.nio.file.AccessDeniedException;
 import java.security.Principal;
 import java.util.List;
 
 @RequestMapping("/api/v1/schedules")
-@Tag(name = "schedule")
+@Tag(name = "schedule", description = "requests to manage schedules")
 interface ScheduleApi {
 
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
-    @Operation(security = {@SecurityRequirement(name = "bearer-key")})
+    @Operation(summary = "returns all schedulers in database",
+            description = "Returns all schedulers in database. Operation available only to the administrator.",
+            security = {@SecurityRequirement(name = "bearer-key")})
+    @ApiResponse(responseCode = "200", description = "Successful operation.",
+            content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ScheduleResponse.class))))
+    @ApiResponse(responseCode = "401", description = "Operation available only to the authenticated user.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    @ApiResponse(responseCode = "403", description = "Operation available only for the administrator.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
     List<ScheduleResponse> getAll();
 
     @GetMapping("/{id}")
     @PreAuthorize("isAuthenticated()")
-    @Operation(security = {@SecurityRequirement(name = "bearer-key")})
-    ScheduleResponse getById(@PathVariable Long id) throws EntityNotFoundException;
+    @Operation(summary = "returns selected schedule by ID",
+            description = "Returns selected schedule by ID. Operation available only for the schedule owner.",
+            security = {@SecurityRequirement(name = "bearer-key")})
+    @ApiResponse(responseCode = "200", description = "Successful operation.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ScheduleResponse.class)))
+    @ApiResponse(responseCode = "401", description = "Operation available only to the authenticated user.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    @ApiResponse(responseCode = "403", description = "Operation available only for the schedule owner.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    @ApiResponse(responseCode = "404", description = "The object with the specified ID does not exist in the system.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    ScheduleResponse getById(
+            @PathVariable @Parameter(description = "Schedule ID to fetch.", example = "1") Long id, Principal principal
+    ) throws EntityNotFoundException;
 
     @PostMapping
     @PreAuthorize("isAuthenticated()")
-    @Operation(security = {@SecurityRequirement(name = "bearer-key")})
-    ScheduleResponse save(@RequestBody @Valid NewSchedule newSchedule, Principal principal)
-            throws UsernameNotFoundException, ResourceAlreadyExistsException;
+    @Operation(summary = "creates a new schedule owned by the user making the request",
+            description = "Creates a new schedule owned by the user making the request.",
+            security = {@SecurityRequirement(name = "bearer-key")})
+    @ApiResponse(responseCode = "201", description = "Successfully created.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ScheduleResponse.class)))
+    @ApiResponse(responseCode = "400", description = "User not found based on authorization data.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    @ApiResponse(responseCode = "401", description = "Operation available only to the authenticated user.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    @ApiResponse(responseCode = "404", description = "Error reported during the validation of the request body.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    @ApiResponse(responseCode = "409", description = "Each user's schedule must have a unique name.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    ScheduleResponse save(
+            @RequestBody @Valid @Parameter(
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = NewSchedule.class)),
+                    description = "New schedule to create."
+            ) NewSchedule newSchedule,
+            Principal principal) throws ResourceAlreadyExistsException;
 
     @DeleteMapping("{id}")
     @PreAuthorize("isAuthenticated()")
-    @Operation(security = {@SecurityRequirement(name = "bearer-key")})
-    void delete(@PathVariable Long id, Principal principal)
-            throws UsernameNotFoundException, EntityNotFoundException, AccessDeniedException;
+    @Operation(summary = "removes existing schedule",
+            description = "Removes existing schedule. Operation available only for schedule owner.",
+            security = {@SecurityRequirement(name = "bearer-key")})
+    @ApiResponse(responseCode = "200", description = "Successfully removed.")
+    @ApiResponse(responseCode = "401", description = "Operation available only to the authenticated user.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    @ApiResponse(responseCode = "403", description = "Operation available only for the schedule owner.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    @ApiResponse(responseCode = "404", description = "The object with the specified ID does not exist in the system.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    void delete(
+            @PathVariable @Parameter(description = "Schedule ID to remove.", example = "1") Long id, Principal principal
+    ) throws EntityNotFoundException, AccessDeniedException;
 }
